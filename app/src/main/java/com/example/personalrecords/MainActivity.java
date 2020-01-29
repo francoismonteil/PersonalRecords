3dbda820002be460ffc3767b40cae234069c7148
Francois <francois.monteil@ig2i.centralelille.fr>
2020-01-29 10:43:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/personalrecords/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.personalrecords;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                Intent toCalandarApplication = new Intent(this, CalandarActivity.class);
+                startActivity(toCalandarApplication);
+            break;
+        }
+    }
+}
+
